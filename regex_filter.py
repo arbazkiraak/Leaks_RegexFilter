07bb275d0b2faf23b4670e2c54dee10076e076f0
Arbaz Hussain <13177578+arbazkiraak@users.noreply.github.com>
2018-12-14 23:21:11 +0530

    Update regex_filter.py

--- regex_filter.py
+++ regex_filter.py
@@ -1,14 +1,18 @@
 import requests,json,re
 from collections import defaultdict
 from collections import Counter
+import queue,threading
 
 class RegexFilter:
+    THREAD_NUM = 5
     def __init__(self,inputfile,ftype,regexdict):
         self.inputfile = inputfile
         self.regexdict = regexdict
         self.ftype = ftype
         self.final_result = defaultdict(list)
         self.final_output = {}
+        self.q = queue.Queue()
+        self.threads = []
         
         if self.ftype == 'offline' and isinstance(self.inputfile,dict):
             for each_file,each_url in self.inputfile.items():
@@ -25,18 +29,31 @@ class RegexFilter:
                             
         elif self.ftype == 'online':
             for each_url in self.inputfile:
-                req = requests.get(each_url)
-                for description,pattern in regexdict.items():
-                    pattern =  r"{}".format(pattern)
-                    regex = re.compile(pattern).findall(req.text)
-                    if len(regex) > 0:
-                        for res in regex:
-                            self.final_result[str(each_url)].append(description) 
-            self.output()
-        else:
-            exit("wrong option buddy!")
+                self.q.put(each_url)
+            for j in range(RegexFilter.THREAD_NUM): 
+                t = threading.Thread(target=self.ProcessQueue)
+                self.threads.append(t)
+                t.start()
+                
+    def ProcessQueue(self):
+        while not self.q.empty():
+            each_url = self.q.get()
+            self.Online_Regexer(each_url)
+            self.q.task_done()
+                
+               
+    def Online_Regexer(self,each_url):
+        req = requests.get(each_url)
+        for description,pattern in regexdict.items():
+            pattern =  r"{}".format(pattern)
+            regex = re.compile(pattern).findall(req.text)
+            if len(regex) > 0:
+                for res in regex:
+                    self.final_result[str(each_url)].append(description) 
         
     def output(self):
+        for t in self.threads:
+            t.join()
         for each_url,each_res in self.final_result.items():
             pattern_match_count = dict(Counter(each_res))
             self.final_output[each_url] = pattern_match_count
